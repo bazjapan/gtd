0d8e55fb7bd4ac3785b9badfd29cb506eba6bb76
bazjapan <bazjapan@users.noreply.github.com>
2020-06-09 19:54:19 +0900

    Create test.hvh.js

---
+++ test.hvh.js
@@ -0,0 +1,12 @@
+FRZ.exports(function(){
+  return function(app){
+      var api = {};
+      
+			api.render = function(item){
+				return dom.div(null, "I live on git is hv a bad thing for item viewer - should they not follow a hvh??? - I think so");
+			}
+			return api;
+  
+  }
+
+})
